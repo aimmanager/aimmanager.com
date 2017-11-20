689b24d8d23d1be678bc79bfe733098c28e87794
Mario Young <maye.co@gmail.com>
2017-11-20 19:02:28 -0300

    new site version

---
+++ js/main.js
@@ -0,0 +1,26 @@
+
+	$().ready(function() {
+
+
+		$('.form-contact').each(function() {
+			$(this).validate();
+		});
+
+		$(".owl-carousel").owlCarousel({items:1, loop:true, autoplay:true});
+
+		$(".logo_clients").owlCarousel({items:1, loop:true, autoplay:true});
+
+
+
+
+        window.addEventListener('scroll', function(e) {
+            var distanceY = window.pageYOffset || document.documentElement.scrollTop,
+                    shrinkOn = 500;
+            if (distanceY > shrinkOn) {
+                $('#nav_bar_top').fadeIn(10);
+            } else {
+                $('#nav_bar_top').fadeOut();
+            }
+        });
+
+  })

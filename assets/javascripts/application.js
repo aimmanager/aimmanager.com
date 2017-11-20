f1a7f4506c9cb4e6092434e6cad9e160311c7176
Mario Young <maye.co@gmail.com>
2017-11-20 19:13:15 -0300

    new site version / 3

--- assets/javascripts/application.js
+++ assets/javascripts/application.js
@@ -1,6 +1,4 @@
-
 	$().ready(function() {
-
 
 		$('.form-contact').each(function() {
 			$(this).validate();
@@ -10,17 +8,13 @@
 
 		$(".logo_clients").owlCarousel({items:1, loop:true, autoplay:true});
 
-
-
-
-        window.addEventListener('scroll', function(e) {
-            var distanceY = window.pageYOffset || document.documentElement.scrollTop,
-                    shrinkOn = 500;
-            if (distanceY > shrinkOn) {
-                $('#nav_bar_top').fadeIn(10);
-            } else {
-                $('#nav_bar_top').fadeOut();
-            }
-        });
+		window.addEventListener('scroll', function(e) {
+			var distanceY = window.pageYOffset || document.documentElement.scrollTop, shrinkOn = 500;
+			if (distanceY > shrinkOn) {
+				$('#nav_bar_top').fadeIn(10);
+			} else {
+				$('#nav_bar_top').fadeOut();
+			}
+		});
 
   })

120be39001a51eb9760a313936febc2573320cd4
Mario Young <maye.co@gmail.com>
2017-11-20 22:33:59 -0300

    reformat

--- assets/javascripts/application_v2.js
+++ assets/javascripts/application_v2.js
@@ -1,28 +1,30 @@
-	$().ready(function() {
+$().ready(function () {
 
-		$('.form-contact').each(function() {
-			$(this).validate();
-		});
+    $('.form-contact').each(function () {
+        $(this).validate();
+    });
 
-		$(".clients_logo_owl").owlCarousel({items:4, autoplay:true, loop:true,
-			responsive:{
-				0: {
-					items:1
-				},
-				992: {
-					items:4
-				}
-		}});
+    $(".clients_logo_owl").owlCarousel({
+        items: 4, autoplay: true, loop: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            992: {
+                items: 4
+            }
+        }
+    });
 
-		$(".jumbotron_phone_image_owl").owlCarousel({autoplay: true, loop:true, items : 1});
+    $(".jumbotron_phone_image_owl").owlCarousel({autoplay: true, loop: true, items: 1});
 
-		window.addEventListener('scroll', function(e) {
-			var distanceY = window.pageYOffset || document.documentElement.scrollTop, shrinkOn = 500;
-			if (distanceY > shrinkOn) {
-				$('#nav_bar_top').fadeIn(10);
-			} else {
-				$('#nav_bar_top').fadeOut(10);
-			}
-		});
+    window.addEventListener('scroll', function (e) {
+        var distanceY = window.pageYOffset || document.documentElement.scrollTop, shrinkOn = 500;
+        if (distanceY > shrinkOn) {
+            $('#nav_bar_top').fadeIn(10);
+        } else {
+            $('#nav_bar_top').fadeOut(10);
+        }
+    });
 
-  })
+})
